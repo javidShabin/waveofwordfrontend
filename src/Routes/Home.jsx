495d272fd1ebd9a56d582e8ac09cf4571c42bf70
javid shabin <javid.main.act@gmail.com>
2024-07-14 19:35:31 +0530

    Add headeing for home page

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -4,8 +4,14 @@ import { homeBook } from "../assets";
 export default function Home() {
   return (
     <main>
-      <div className="w-full">
-        <img className="w-full" src={homeBook} alt="" />
+      <div className="w-full h-[400px] overflow-hidden relative">
+        <img className="h-[500px] w-full" src={homeBook} />
+        <div>
+          <h1 className="text-white text-[70px] font-[900] absolute top-[100px] left-8 ">
+            Find Your <br />
+            Books
+          </h1>
+        </div>
       </div>
     </main>
   );
