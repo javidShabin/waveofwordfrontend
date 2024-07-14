1c957c0959dfeaa08a2673fb6f3b38648823d2b5
javid shabin <javid.main.act@gmail.com>
2024-07-14 19:45:21 +0530

    Add books component and fetch data from url, and pass data to books compoennt using props

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import { homeBook } from "../assets";
+import { useLoaderData } from "react-router-dom";
+import Books from "./Books";
+
+export async function loader() {
+  const response = await fetch("http://localhost:5000/books");
+  const books = await response.json();
+  console.log(books);
+  return { books };
+}
 
 export default function Home() {
+  const { books } = useLoaderData();
   return (
     <main>
       <div className="w-full h-[400px] overflow-hidden relative">
@@ -11,6 +21,17 @@ export default function Home() {
             Find Your <br />
             Books
           </h1>
+        </div>
+      </div>
+
+      {/* Books section */}
+      <div className="book">
+        <div className="container">
+          <div>
+            {books.map((book) => {
+              return <Books key={book._id} book={book} />;
+            })}
+          </div>
         </div>
       </div>
     </main>
--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import {
-    createBrowserRouter,
-    RouterProvider,
-  } from "react-router-dom";
-import Root from './Routes/Root.jsx';
-import ErrorPage from './Error-page.jsx';
-import Home from './Routes/Home.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./Routes/Root.jsx";
+import ErrorPage from "./Error-page.jsx";
+import Home, {loader as homeLoader} from "./Routes/Home.jsx";
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Root/>,
-      errorElement: <ErrorPage/>,
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
 
-      children: [
-        {
-            path: "/",
-            element: <Home/>
-        }
-      ]
-    },
-  ]);
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        loader: homeLoader
+      },
+    ],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
