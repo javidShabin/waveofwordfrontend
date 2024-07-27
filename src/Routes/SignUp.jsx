b6492578d92d9eb42dae2ddd1cab125ebad44177
javid shabin <javid.main.act@gmail.com>
2024-07-27 18:33:51 +0530

    npm i react hook form and create signup form compoennet add react hook form basic structur

--- src/Routes/SignUp.jsx
+++ src/Routes/SignUp.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import SignupForm from '../component/SignUpForm'
 
 export default function SignUp() {
   return (
     <div className='container mx-auto px-10'>
       <section>
         <h1 className='text-[25px] font-bold'>Sign Up</h1>
+        <SignupForm/>
       </section>
     </div>
   )
