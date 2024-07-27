6b45d7dd939c786530a6a509a53c3c5fe385d9ed
javid shabin <javid.main.act@gmail.com>
2024-07-27 18:46:13 +0530

    Make needed changes the signup form and add styles

--- src/Routes/SignUp.jsx
+++ src/Routes/SignUp.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import SignupForm from '../component/SignUpForm'
+import React from "react";
+import SignupForm from "../component/SignUpForm";
 
 export default function SignUp() {
   return (
-    <div className='container mx-auto px-10'>
-      <section>
-        <h1 className='text-[25px] font-bold'>Sign Up</h1>
-        <SignupForm/>
+    <main className="container mx-auto px-10">
+      <section className="">
+        <h2 className="text-3xl font-bold">SignUp</h2>
+        <div className="mx-auto flex justify-center items-center">
+          <SignupForm />
+        </div>
       </section>
-    </div>
-  )
+    </main>
+  );
 }
--- src/component/SignUpForm.jsx
+++ src/component/SignUpForm.jsx
@@ -1,35 +1,55 @@
-import { useForm } from "react-hook-form"
-
+import { useForm } from "react-hook-form";
 
 export default function SignupForm() {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
-  } = useForm()
+  } = useForm();
 
-
-  const onSubmit = (data) => console.log(data)
-
-
-  console.log(watch("example")) // watch input value by passing the name of it
-
+  const onSubmit = (data) => {};
 
   return (
     /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-    <form onSubmit={handleSubmit(onSubmit)}>
-      {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue="test" {...register("example")} />
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col gap-2 mt-4 w-[1000px]"
+    >
+      <label htmlFor="name">Name</label>
+      <input
+        className="mb-2 border border-indigo-600 py-2 rounded-md"
+        type="text"
+        id="name"
+        {...register("name", { required: true })}
+      />
 
+      {errors.name && <span>This field is required</span>}
 
-      {/* include validation with required or other standard HTML validation rules */}
-      <input {...register("exampleRequired", { required: true })} />
-      {/* errors will return when field validation fails  */}
-      {errors.exampleRequired && <span>This field is required</span>}
+      <label htmlFor="email">Email</label>
+      <input
+        className="mb-2 border border-indigo-600 py-2 rounded-md"
+        type="email"
+        id="email"
+        {...register("email", { required: true })}
+      />
 
+      {errors.email && <span>This field is required</span>}
 
-      <input type="submit" />
+      <label htmlFor="password">Password</label>
+      <input
+        className="mb-2 border border-indigo-600 py-2 rounded-md"
+        type="password"
+        id="password"
+        {...register("password", { required: true })}
+      />
+
+      {errors.password && <span>This field is required</span>}
+
+      {/* Sign up button */}
+      <input
+        className="mt-6 p-2 bg-indigo-800 text-white rounded-md"
+        type="submit"
+      />
     </form>
-  )
+  );
 }
