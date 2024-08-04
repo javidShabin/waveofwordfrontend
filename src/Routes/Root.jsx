c7a0aee0b21eab4277ddaca4fe91647e8c1ca2c7
javid shabin <javid.main.act@gmail.com>
2024-08-04 17:57:03 +0530

    If login user show logout in header if not, show login. Its created use with redux

--- src/Routes/Root.jsx
+++ src/Routes/Root.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link, Outlet } from "react-router-dom";
 
 export default function Root() {
+    const loggedIn = useSelector(state => state.login.loggedIn)
   return (
     <>
       <header className="shadow-lg flex justify-center py-6 w-full z-[999] bg-white sticky top-0 left-0">
@@ -15,9 +17,13 @@ export default function Root() {
               <li>
                 <Link to={"/author"}>Authors</Link>
               </li>
-              <li>
-                <Link to={"/signup"}>Sign Up</Link>
+              {
+                loggedIn? <li>
+                <Link to={"/logout"}>Logout</Link>
+              </li>: <li>
+                <Link to={"/login"}>Login</Link>
               </li>
+              }
             </ul>
           </nav>
         </div>
